5a190fef5ec015c7b8338e7783b41e2f0c3637bd
meets1 <88488646+meets1@users.noreply.github.com>
2021-09-03 16:46:59 +0530

    Update actionCreators.js

--- src/redux/actions/actionCreators.js
+++ src/redux/actions/actionCreators.js
@@ -4,7 +4,6 @@ export const addUser = (email, password) => async (dispatch)=>{
     if (email && password) {
         localStorage.setItem('email', email);
         localStorage.setItem('password', password);
-        // localStorage.setItem('password', password);
         // setData([...data, { email: email, password: password }]) 
         // setEmail('');
         // setPassword(''); 
@@ -25,13 +24,3 @@ export const actionData = (data) => {
         payload: data
     }
 }
-
-// const SIGN_UP = SIGN_UP;
-
-// dispatch({type:SIGN_UP,payload:data})
-
-// export const actionCreators = {
-//     userSignup: (guest) => ({ type: Types.REGISTER, payload: { guest } }),
-//     signupSuccess: (user)=>({type: Types.REGISTER_SUCCESS,payload:{user}}),
-//     userLogin:(user)=>({type:Types.LOGIN_SUCCESS,payload:{user}})
-// }
